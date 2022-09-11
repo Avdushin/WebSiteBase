fb4e69fdbfb78ebfe26dc1fb1060bd552ac4cc31
avdushin <avdushinbeaver1@gmail.com>
2022-09-11 19:37:55 +0300

    Pages fixed

--- Makefile
+++
@@ -1,5 +0,0 @@
-setup:
-	powershell start ./install/node-v16.17.0-x64.msi
-
-clean: 
-	rmdir /s /q dist
--- init.bat
+++
@@ -1,2 +0,0 @@
-npm init -y
-npm i -D gulp gulp-file-include gulp-remove-empty-lines gulp-csso browser-sync del gulp-htmlmin gulp-replace fs
